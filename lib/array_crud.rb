26e64d7cb26f3a932a167d366eddfcb5c01fac90
Yoshi Mejia <yoshimejia00@gmail.com>
2020-08-04 15:44:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ my_array = ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-my_array >> "element"
+array >> ["element"]
 end
 
 def add_element_to_start_of_array(array, element)
